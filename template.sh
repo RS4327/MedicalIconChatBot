e09ddb347468f25dc42249ddc30380d34f2ac372
RSR4327 <raju.meenige@gmail.com>
2025-09-27 16:53:45 +0530

    Folders added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating the directories 
+mkdir -p src
+mkdir -p research
+# Creating the Files
+touch src/__init__.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trail.ipynb
+touch requirements.txt
+
+
+echo 'Directories and files created successfully'
