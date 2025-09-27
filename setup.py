c461d5c4a0bb4da05a4c4b415b5d3c4cbf351d68
RSR4327 <raju.meenige@gmail.com>
2025-09-27 17:41:27 +0530

    Commited the Requirements and setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="Medical Icon ChatBot",
+    version="1.0.0",
+    author='Raju Minega',
+    author_email="raju.meenige@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
